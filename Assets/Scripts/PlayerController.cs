b925234ca918bcb4af1bd5bbb4d6b6f6caa4de17
Fernando Ruiz Gimenez <fer.ruizgimenez@gmail.com>
2024-05-06 20:55:05 +0200

    #2 Player Controller
    - Follow SOLID principles and ensure each script/component handles at least one functionality
    - Creation of the player movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Variables
+    private bool isMoving = true;
+    [SerializeField]
+    private float moveSpeed;
+    private void Update()
     {
-        
+        if(isMoving)
+        {
+            SetMovement();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void SetMovement()
     {
-        
+        float inputX = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * (inputX * moveSpeed * Time.deltaTime);
     }
 }
