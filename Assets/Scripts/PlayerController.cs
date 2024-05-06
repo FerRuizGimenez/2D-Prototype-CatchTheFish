983a066ee99cb27916384062b9077329df6a7626
Fernando Ruiz Gimenez <fer.ruizgimenez@gmail.com>
2024-05-06 21:55:04 +0200

    #3 Player Camera Limits
    - Restriction of the screen considering the Viewport, obtaining the right and left limits of the screen
    - Optimization of the Player Controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,39 @@ public class PlayerController : MonoBehaviour
     private bool isMoving = true;
     [SerializeField]
     private float moveSpeed;
+    private Transform  myTransform;
+
+    private float leftViewPortLimit;
+    private float rightViewPortLimit;
+    private float playerOffset = 1.1f;
+
+    private void Start()
+    {
+        myTransform = GetComponent<Transform>();
+        // Almaceno la posicion del mundo con respecto al ViewPort
+        leftViewPortLimit = Camera.main.ViewportToWorldPoint(Vector3.zero).x + playerOffset;
+        rightViewPortLimit = Camera.main.ViewportToWorldPoint(Vector3.right).x - playerOffset;
+    }
     private void Update()
     {
         if(isMoving)
         {
             SetMovement();
         }
+        if (myTransform.position.x > rightViewPortLimit)
+            SetPlayerLimitPosition(rightViewPortLimit);
+        if (myTransform.position.x < leftViewPortLimit)
+            SetPlayerLimitPosition(leftViewPortLimit);
     }
+
+    private void SetPlayerLimitPosition(float limit)
+    {
+        myTransform.position = new Vector3(limit, transform.position.y, 0);
+    }
+
     private void SetMovement()
     {
         float inputX = Input.GetAxis("Horizontal");
-        transform.position += Vector3.right * (inputX * moveSpeed * Time.deltaTime);
+        myTransform.position += Vector3.right * (inputX * moveSpeed * Time.deltaTime);
     }
 }
