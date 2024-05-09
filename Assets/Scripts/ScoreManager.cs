be0a25b25f98233f2ca2a9b9b5e067c43c8836ed
Fernando Ruiz Gimenez <fer.ruizgimenez@gmail.com>
2024-05-09 13:01:30 +0200

    #7 Score TEXT UI
    - Displaying score on the screen through the score manager.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     private int score;
+    private TextMeshProUGUI scoreTextUI;
     private void Awake()
     {
         instance = this;
+        scoreTextUI = GameObject.Find("ScoreTextUI").GetComponent<TextMeshProUGUI>();
     }
     public void AddScore()
     {
         score++;
-        Debug.Log("He sumado puntos: " + score);
+        AddScoreUI();
+    }
+    private void AddScoreUI()
+    {
+        scoreTextUI.text = score.ToString();
     }
 }
