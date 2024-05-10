64c469f338a9d4aac2b8201f8b18a887a0835a07
Fernando Ruiz Gimenez <fer.ruizgimenez@gmail.com>
2024-05-10 18:34:20 +0200

    #12 Main Menu
    - Simple design of the main menu.

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -9,11 +9,21 @@ public class LivesManager : MonoBehaviour
     private int livesCounter = 5;
     private GameObject[] livesImageUI;
     private Color uIImageColor;
+    private GameObject livesPanel;
     private void Awake()
     {
         instance = this;
-        livesImageUI = GameObject.FindGameObjectsWithTag("LifeImageUI");
+        //livesImageUI = GameObject.FindGameObjectsWithTag("LifeImageUI");
         uIImageColor = new Color(1, 1, 1, 0.5f);
+    }
+    private void Start()
+    {
+        livesPanel = GameObject.Find("LivesPanel");
+        livesImageUI = new GameObject[livesPanel.transform.childCount];
+        for (int i = 0; i < livesPanel.transform.childCount; i++)
+        {
+            livesImageUI[i] = GameObject.Find("Life" + (i + 1));
+        } 
     }
     public void RemoveLife()
     {
